cb01a236ccd9333e2417935ba705e6fb6f5af6ce
Natalia Derbeneva <nat_ff@mail.ru>
2020-04-06 05:31:40 +0400

    fix: change functions Shift, Alt, Ctrl to correct behavior

--- script.js
+++ script.js
@@ -1,68 +1,68 @@
 const keys = {
-    'Backquote': {en: '\`', enShift: '~', ru: 'ё', ruShift: "Ё", type: "print"},
-    'Digit1': {en: '1', enShift: '!', ru: '1', ruShift: '!', type: "print"},
-    'Digit2': {en: '2', enShift: '@', ru: '2', ruShift: '\"', type: "print"},
-    'Digit3': {en: '3', enShift: '#', ru: '3', ruShift: '№', type: "print"},
-    'Digit4': {en: '4', enShift: '$', ru: '4', ruShift: ';', type: "print"},
-    'Digit5': {en: '5', enShift: '%', ru: '5', ruShift: '%', type: "print"},
-    'Digit6': {en: '6', enShift: '^', ru: '6', ruShift: ':', type: "print"},
-    'Digit7': {en: '7', enShift: '&', ru: '7', ruShift: '?', type: "print"},
-    'Digit8': {en: '8', enShift: '*', ru: '8', ruShift: '*', type: "print"},
-    'Digit9': {en: '9', enShift: '(', ru: '9', ruShift: '(', type: "print"},
-    'Digit0': {en: '0', enShift: ')', ru: '0', ruShift:')', type: "print"},
-    'Minus': {en: '-', enShift: '_', ru: '-', ruShift:'_', type: "print"},
-    'Equal': {en: '=', enShift: '+', ru: '=', ruShift:'+', type: "print"},
-    'Backspace': {en: 'Backspace', enShift: 'Backspace', ru: 'Backspace', ruShift:'Backspace', type: "action"},
-    'Tab': {en: 'Tab', enShift: 'Tab', ru: 'Tab', ruShift:'Tab', type: "action"},
-    'KeyQ': {en: 'q', enShift: 'Q', ru: 'й', ruShift: 'Й', type: "print"},
-    'KeyW': {en: 'w', enShift: 'W', ru: 'ц', ruShift: 'Ц', type: "print"},
-    'KeyE': {en: 'e', enShift: 'E', ru: 'у', ruShift: 'У', type: "print"},
-    'KeyR': {en: 'r', enShift: 'R', ru: 'к', ruShift: 'К', type: "print"},
-    'KeyT': {en: 't', enShift: 'T', ru: 'е', ruShift: 'Е', type: "print"},
-    'KeyY': {en: 'y', enShift: 'Y', ru: 'н', ruShift: 'Н', type: "print"},
-    'KeyU': {en: 'u', enShift: 'U', ru: 'г', ruShift: 'Г', type: "print"},
-    'KeyI': {en: 'i', enShift: 'I', ru: 'ш', ruShift: 'Ш', type: "print"},
-    'KeyO': {en: 'o', enShift: 'O', ru: 'щ', ruShift: 'Щ', type: "print"},
-    'KeyP': {en: 'p', enShift: 'P', ru: 'з', ruShift: 'З', type: "print"},
-    'BracketRight': {en: '[', enShift: '{', ru: 'х', ruShift: 'Х', type: "print"},
-    'BracketLeft': {en: ']', enShift: '}', ru: 'ъ', ruShift: 'Ъ', type: "print"},
-    'Backslash': {en: '\\', enShift: '|', ru: '\\', ruShift: '/', type: "print"},
-    'Delete': {en: 'Delete', enShift: 'Delete', ru: 'Delete', ruShift: 'Delete', type: "action"},
-    'CapsLock': {en: 'CapsLock', enShift: 'CapsLock', ru: 'CapsLock', ruShift: 'CapsLock',  type: "action"},
-    'KeyA': {en: 'a', enShift: 'A', ru: 'ф', ruShift: 'Ф', type: "print"},
-    'KeyS': {en: 's', enShift: 'S', ru: 'ы', ruShift: 'Ы', type: "print"},
-    'KeyD': {en: 'd', enShift: 'D', ru: 'в', ruShift: 'В', type: "print"},
-    'KeyF': {en: 'f', enShift: 'F', ru: 'а', ruShift: 'А', type: "print"},
-    'KeyG': {en: 'g', enShift: 'G', ru: 'п', ruShift: 'П', type: "print"},
-    'KeyH': {en: 'h', enShift: 'H', ru: 'р', ruShift: 'Р', type: "print"},
-    'KeyJ': {en: 'j', enShift: 'J', ru: 'о', ruShift: 'О', type: "print"},
-    'KeyK': {en: 'k', enShift: 'K', ru: 'л', ruShift: 'Л', type: "print"},
-    'KeyL': {en: 'l', enShift: 'L', ru: 'д', ruShift: 'Д', type: "print"},
-    'Semicolon': {en: ';', enShift: ':', ru: 'ж', ruShift: 'Ж', type: "print"},
-    'Quote': {en: '\'', enShift: '"', ru: 'э', ruShift: 'Э', type: "print"},
-    'Enter': {en: 'Enter', enShift: 'Enter', ru: 'Enter', ruShift: 'Enter', type: "action"},
-    'ShiftLeft': {en: 'Shift', enShift: 'Shift', ru: 'Shift', ruShift: 'Shift', type: "action"},
-    'KeyZ': {en: 'z', enShift: 'Z', ru: 'я', ruShift: 'Я', type: "print"},
-    'KeyX': {en: 'x', enShift: 'X', ru: 'ч', ruShift: 'Ч', type: "print"},
-    'KeyC': {en: 'c', enShift: 'C', ru: 'с', ruShift: 'С', type: "print"},
-    'KeyV': {en: 'v', enShift: 'V', ru: 'м', ruShift: 'М', type: "print"},
-    'KeyB': {en: 'b', enShift: 'B', ru: 'и', ruShift: 'И', type: "print"},
-    'KeyN': {en: 'n', enShift: 'N', ru: 'т', ruShift: 'Т', type: "print"},
-    'KeyM': {en: 'm', enShift: 'M', ru: 'ь', ruShift: 'Ь', type: "print"},
-    'Key,': {en: ',', enShift: '<', ru: 'б', ruShift: 'Б', type: "print"},
-    'Key.': {en: '.', enShift: '>', ru: 'ю', ruShift: 'Ю', type: "print"},
-    'Key/': {en: '/', enShift: '?', ru: '.', ruShift: ',', type: "print"},
-    'ArrowUp': {en: '&uarr;', enShift: '&uarr;', ru: '&uarr;', ruShift: '&uarr;', type: "action"},
-    'ShiftRight': {en: 'Shift', enShift: 'Shift', ru: 'Shift', ruShift: 'Shift', type: "action"},
-    'ControlLeft': {en: 'Ctrl', enShift: 'Ctrl', ru: 'Ctrl', ruShift: 'Ctrl', type: "action"},
-    'MetaLeft': {en: 'Win', enShift: 'Win', ru: 'Win', ruShift: 'Win', type: "action"},
-    'AltLeft': {en: 'Alt', enShift: 'Alt', ru: 'Alt', ruShift: 'Alt', type: "action"},
-    'Space': {en: " ", enShift: " ", ru: " ", ruShift: " ", type: "action"},
-    'AltRight': {en: 'Alt', enShift: 'Alt', ru: 'Alt', ruShift: 'Alt', type: "action"},
-    'ArrowLeft': {en: '&larr;', enShift: '&larr;', ru: '&larr;', ruShift: '&larr;', type: "action"},
-    'ArrowDown': {en: '&darr;', enShift: '&darr;', ru: '&darr;', ruShift: '&darr;', type: "action"},
-    'ArrowRight': {en: '&rarr;', enShift: '&rarr;', ru: '&rarr;', ruShift: '&rarr;', type: "action"},
-    'ControlRight': {en: 'Ctrl', enShift: 'Ctrl', ru: 'Ctrl', ruShift: 'Ctrl', type: "action"},
+    'Backquote': {en: '\`', enUp: '~', ru: 'ё', ruUp: "Ё", type: "print"},
+    'Digit1': {en: '1', enUp: '!', ru: '1', ruUp: '!', type: "print"},
+    'Digit2': {en: '2', enUp: '@', ru: '2', ruUp: '\"', type: "print"},
+    'Digit3': {en: '3', enUp: '#', ru: '3', ruUp: '№', type: "print"},
+    'Digit4': {en: '4', enUp: '$', ru: '4', ruUp: ';', type: "print"},
+    'Digit5': {en: '5', enUp: '%', ru: '5', ruUp: '%', type: "print"},
+    'Digit6': {en: '6', enUp: '^', ru: '6', ruUp: ':', type: "print"},
+    'Digit7': {en: '7', enUp: '&', ru: '7', ruUp: '?', type: "print"},
+    'Digit8': {en: '8', enUp: '*', ru: '8', ruUp: '*', type: "print"},
+    'Digit9': {en: '9', enUp: '(', ru: '9', ruUp: '(', type: "print"},
+    'Digit0': {en: '0', enUp: ')', ru: '0', ruUp:')', type: "print"},
+    'Minus': {en: '-', enUp: '_', ru: '-', ruUp:'_', type: "print"},
+    'Equal': {en: '=', enUp: '+', ru: '=', ruUp:'+', type: "print"},
+    'Backspace': {en: 'Backspace', enUp: 'Backspace', ru: 'Backspace', ruUp:'Backspace', type: "action"},
+    'Tab': {en: 'Tab', enUp: 'Tab', ru: 'Tab', ruUp:'Tab', type: "action"},
+    'KeyQ': {en: 'q', enUp: 'Q', ru: 'й', ruUp: 'Й', type: "print"},
+    'KeyW': {en: 'w', enUp: 'W', ru: 'ц', ruUp: 'Ц', type: "print"},
+    'KeyE': {en: 'e', enUp: 'E', ru: 'у', ruUp: 'У', type: "print"},
+    'KeyR': {en: 'r', enUp: 'R', ru: 'к', ruUp: 'К', type: "print"},
+    'KeyT': {en: 't', enUp: 'T', ru: 'е', ruUp: 'Е', type: "print"},
+    'KeyY': {en: 'y', enUp: 'Y', ru: 'н', ruUp: 'Н', type: "print"},
+    'KeyU': {en: 'u', enUp: 'U', ru: 'г', ruUp: 'Г', type: "print"},
+    'KeyI': {en: 'i', enUp: 'I', ru: 'ш', ruUp: 'Ш', type: "print"},
+    'KeyO': {en: 'o', enUp: 'O', ru: 'щ', ruUp: 'Щ', type: "print"},
+    'KeyP': {en: 'p', enUp: 'P', ru: 'з', ruUp: 'З', type: "print"},
+    'BracketRight': {en: '[', enUp: '{', ru: 'х', ruUp: 'Х', type: "print"},
+    'BracketLeft': {en: ']', enUp: '}', ru: 'ъ', ruUp: 'Ъ', type: "print"},
+    'Backslash': {en: '\\', enUp: '|', ru: '\\', ruUp: '/', type: "print"},
+    'Delete': {en: 'Delete', enUp: 'Delete', ru: 'Delete', ruUp: 'Delete', type: "action"},
+    'CapsLock': {en: 'CapsLock', enUp: 'CapsLock', ru: 'CapsLock', ruUp: 'CapsLock',  type: "action"},
+    'KeyA': {en: 'a', enUp: 'A', ru: 'ф', ruUp: 'Ф', type: "print"},
+    'KeyS': {en: 's', enUp: 'S', ru: 'ы', ruUp: 'Ы', type: "print"},
+    'KeyD': {en: 'd', enUp: 'D', ru: 'в', ruUp: 'В', type: "print"},
+    'KeyF': {en: 'f', enUp: 'F', ru: 'а', ruUp: 'А', type: "print"},
+    'KeyG': {en: 'g', enUp: 'G', ru: 'п', ruUp: 'П', type: "print"},
+    'KeyH': {en: 'h', enUp: 'H', ru: 'р', ruUp: 'Р', type: "print"},
+    'KeyJ': {en: 'j', enUp: 'J', ru: 'о', ruUp: 'О', type: "print"},
+    'KeyK': {en: 'k', enUp: 'K', ru: 'л', ruUp: 'Л', type: "print"},
+    'KeyL': {en: 'l', enUp: 'L', ru: 'д', ruUp: 'Д', type: "print"},
+    'Semicolon': {en: ';', enUp: ':', ru: 'ж', ruUp: 'Ж', type: "print"},
+    'Quote': {en: '\'', enUp: '"', ru: 'э', ruUp: 'Э', type: "print"},
+    'Enter': {en: 'Enter', enUp: 'Enter', ru: 'Enter', ruUp: 'Enter', type: "action"},
+    'ShiftLeft': {en: 'Shift', enUp: 'Shift', ru: 'Shift', ruUp: 'Shift', type: "keyPress"},
+    'KeyZ': {en: 'z', enUp: 'Z', ru: 'я', ruUp: 'Я', type: "print"},
+    'KeyX': {en: 'x', enUp: 'X', ru: 'ч', ruUp: 'Ч', type: "print"},
+    'KeyC': {en: 'c', enUp: 'C', ru: 'с', ruUp: 'С', type: "print"},
+    'KeyV': {en: 'v', enUp: 'V', ru: 'м', ruUp: 'М', type: "print"},
+    'KeyB': {en: 'b', enUp: 'B', ru: 'и', ruUp: 'И', type: "print"},
+    'KeyN': {en: 'n', enUp: 'N', ru: 'т', ruUp: 'Т', type: "print"},
+    'KeyM': {en: 'm', enUp: 'M', ru: 'ь', ruUp: 'Ь', type: "print"},
+    'Key,': {en: ',', enUp: '<', ru: 'б', ruUp: 'Б', type: "print"},
+    'Key.': {en: '.', enUp: '>', ru: 'ю', ruUp: 'Ю', type: "print"},
+    'Key/': {en: '/', enUp: '?', ru: '.', ruUp: ',', type: "print"},
+    'ArrowUp': {en: '&uarr;', enUp: '&uarr;', ru: '&uarr;', ruUp: '&uarr;', type: "action"},
+    'ShiftRight': {en: 'Shift', enUp: 'Shift', ru: 'Shift', ruUp: 'Shift', type: "keyPress"},
+    'ControlLeft': {en: 'Ctrl', enUp: 'Ctrl', ru: 'Ctrl', ruUp: 'Ctrl', type: "keyPress"},
+    'MetaLeft': {en: 'Win', enUp: 'Win', ru: 'Win', ruUp: 'Win', type: "action"},
+    'AltLeft': {en: 'Alt', enUp: 'Alt', ru: 'Alt', ruUp: 'Alt', type: "keyPress"},
+    'Space': {en: " ", enUp: " ", ru: " ", ruUp: " ", type: "print"},
+    'AltRight': {en: 'Alt', enUp: 'Alt', ru: 'Alt', ruUp: 'Alt', type: "keyPress"},
+    'ArrowLeft': {en: '&larr;', enUp: '&larr;', ru: '&larr;', ruUp: '&larr;', type: "action"},
+    'ArrowDown': {en: '&darr;', enUp: '&darr;', ru: '&darr;', ruUp: '&darr;', type: "action"},
+    'ArrowRight': {en: '&rarr;', enUp: '&rarr;', ru: '&rarr;', ruUp: '&rarr;', type: "action"},
+    'ControlRight': {en: 'Ctrl', enUp: 'Ctrl', ru: 'Ctrl', ruUp: 'Ctrl', type: "keyPress"},
     }
     
 class Button{
@@ -70,19 +70,20 @@ class Button{
 
 constructor(value){
     this.lang = "en";
-    this.shift = "";
+    this.capsLock = "";
     this.textarea = "";
     this.ctrl = false;
     this.alt = false;
+    this.shift = false;
     this.currentPosition = 0;
 
     if(Array.isArray(value)){           
         if(value.length == 1)
             [this.lang] = value;
         else if(value.length == 2)
-            [this.lang, this.shift] = value; 
+            [this.lang, this.capsLock] = value; 
         else if(value.length == 3)
-            [this.lang, this.shift, this.workspace] = value; 
+            [this.lang, this.capsLock, this.workspace] = value; 
     };
 }
 
@@ -90,8 +91,8 @@ set language(lang) {
     this.lang = lang;
 }
 
-set upperCase(shift) {
-    this.shift = shift;
+set upperCase(capsLock) {
+    this.capsLock = capsLock;
 }
 
 set workspace(textarea){
@@ -106,7 +107,7 @@ fillKeyboard() {
     let i = 0;
     let buttons = document.querySelectorAll('.key');
     for (const [keyCode, value] of Object.entries(keys)){ 
-        let {[`${this.lang}${this.shift}`]:innerHTML, type} = value;
+        let {[`${this.lang}${this.capsLock}`]:innerHTML, type} = value;
         buttons[i].id = type;
         buttons[i].name = keyCode; 
         buttons[i].innerHTML = innerHTML;
@@ -142,7 +143,7 @@ PrintSymbol(s){
 }
 
 Print(keyCode){
-    let outStringType = this.lang + this.shift;
+    let outStringType = this.lang + this.capsLock;
     let {[keyCode]: {[outStringType]: value}} = keys;
     this.PrintSymbol(value);
 }
@@ -196,23 +197,41 @@ Enter(){
 }
 
 CapsLock(){
-    if(this.shift) this.shift = "";
-    else this.shift = "Shift";
+    if(this.capsLock) this.capsLock = "";
+    else this.capsLock = "Up";
     this.fillKeyboard();
+    this.textarea.focus();
 }
-
-ShiftLeft(){this.CapsLock();}
-ShiftRight(){this.CapsLock();}
 
 changeLang(){
-    let posInLangs = this.#langs.indexOf(lang);
+    let posInLangs = this.#langs.indexOf(this.lang);
     this.lang = this.#langs[Number(!posInLangs)];
     this.fillKeyboard();
+    this.AltReset();
+    this.ShiftReset();
+    this.textarea.focus();
 }
+
+Shift(){
+    this.shift = true;
+    this.CapsLock();
+    if(this.alt) this.changeLang();
+}
+
+ShiftReset(){
+    this.shift = false;
+    this.CapsLock();
+}
+
+ShiftLeft(){this.Shift();}
+ShiftRight(){this.Shift();}
+ShiftLeftReset(){this.ShiftReset();}
+ShiftRightReset(){this.ShiftReset();}
 
 Alt(){
     this.alt = true;
-    if(this.ctrl) this.changeLang();
+    if(this.shift) this.changeLang();
+    this.textarea.focus();
 }
 
 AltReset(){
@@ -226,11 +245,12 @@ AltRightReset(){this.AltReset();}
 
 Ctrl(){
     this.ctrl = true;
-    if(this.alt) this.changeLang();
+    this.textarea.focus();
 }
 
 CtrlReset(){
     this.ctrl = false;
+    this.textarea.focus();
 }
 
 ControlLeft(){this.Ctrl();}
@@ -363,9 +383,7 @@ ArrowLeft(){
 }      
 
 
-let lang = "en"; 
-let shift = "Shift";
-let btn = new Button([lang, shift]);
+let btn = new Button();
 
 
 (function (){
@@ -377,7 +395,6 @@ bodyWrapper.classList.add('body__wrapper');
 
 let output = document.createElement('textarea');
 output.classList.add('output-textarea');
-output.setAttribute('autofocus','true');
 bodyWrapper.appendChild(output);
 
 let keyboardWrapper = document.createElement('div');
@@ -412,7 +429,9 @@ buttons[42].classList.add('key_long');
 buttons[54].classList.add('key_middle');
 buttons[58].classList.add('key_very-long');
 
+
 btn.fillKeyboard();
+
 btn.workspace = output; 
 btn.Print("KeyZ");
 btn.Print("KeyM");
@@ -434,12 +453,17 @@ btn.Print("KeyP");
 }());
 
 
+let allButtons = document.querySelectorAll('.keyboard__wrapper button');
+allButtons.forEach(button => button.addEventListener('click',someButtonClick));
+
+function someButtonClick(){
+//    console.log(event.target.name);
+}
 
 let actionButtons = document.querySelectorAll('#action');
 actionButtons.forEach(button => button.addEventListener('click',clickActionButton));
 
 function clickActionButton(event){
-    console.log(event.target.name);
     btn[event.target.name]();
 }
 
@@ -447,9 +471,28 @@ let printButtons = document.querySelectorAll('#print');
 printButtons.forEach(button => button.addEventListener('click',clickPrintButton));
 
 function clickPrintButton(event){
-    console.log(event.target.innerHTML);
     btn.PrintSymbol(event.target.innerHTML);
 }
+
+
+let pressButtons = document.querySelectorAll('#keyPress');
+pressButtons.forEach(button => button.addEventListener('click',clickPressButton));
+
+function clickPressButton(event){
+    var button = event.target; 
+
+    var fieldName, lenOfSideName, addFunctionName;
+    button.name.match("Left") ? lenOfSideName = 4 : lenOfSideName = 5;  
+    fieldName = (button.name.substring(0,button.name.length - lenOfSideName)).toLowerCase();
+    if(btn[fieldName]) addFunctionName = "Reset";
+    else addFunctionName = "";
+
+    funcName = `${button.name}${addFunctionName}`;
+    btn[funcName]();
+
+    console.log(button.name);
+}
+
 
 let textarea = document.querySelector('.output-textarea');
 textarea.addEventListener('click',clickTextarea);
